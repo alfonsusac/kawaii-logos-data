9039fc775fb29bfd01e12b557406c1e001242aa1
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-05 17:06:03 +0700

    fix gha

--- src/git-shell.ts
+++ src/git-shell.ts
@@ -2,7 +2,7 @@ import { GitHub } from "./github-url"
 import { rootDir } from "./paths"
 
 export class Git {
-  constructor(public readonly pathToGit: string = "") { }
+  constructor(public readonly pathToGit: string = rootDir) { }
 
   static clone = async (what: string, cwd: string, args?: {
     quiet?: boolean,
