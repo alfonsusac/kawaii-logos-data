8ad90125027973a3ec10fb663ae4563364b1c7e5
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-05 23:24:00 +0700

    Update types

---
+++ src/index.d.ts
@@ -0,0 +1,23 @@
+export type Data = (Author & {
+    images: Image[];
+})[];
+export type Image = {
+    title: string;
+    imgSrc: string;
+    source: string;
+    createdAt?: Date;
+    className?: string;
+};
+export type Author = {
+    handleName: string;
+    pfp?: string;
+    link: {
+        github?: string;
+        twitter?: string;
+    };
+    repository?: string;
+    license?: {
+        label?: string;
+        href?: string;
+    };
+};
