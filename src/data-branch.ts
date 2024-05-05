cf602342bc8687c23e973d36232d0486623bd79f
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-05 18:12:19 +0700

    echo

--- src/data-branch.ts
+++ src/data-branch.ts
@@ -52,7 +52,7 @@ export async function updateDataBranch(data: string, updatedAt: string) {
     // delay
     await new Promise(resolve => setTimeout(resolve, 1000))
 
-    await Bun.$`ls -l`
+    await Bun.$`echo *`
 
     // Save changes to `data` branch
     await git.add("images.json .gitignore")
