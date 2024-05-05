ba4c0be410d40e3c5c3cc8c6181d8e082c1a6518
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-05 23:09:48 +0700

    wow

--- src/update-types.ts
+++ src/update-types.ts
@@ -1,46 +1,38 @@
 import { Git } from "./git-shell"
 import { logError, logProcess } from "./log"
-import { delay, logAndDelay } from "./util"
+import { logAndDelay } from "./util"
 
 let types = await Bun.file(`src/types/index.ts`).text()
 console.log(types)
-
 logAndDelay(`Type file retrieved`)
 
 
 try {
   let branch = await Git.branch({ all: true }).text()
-
   logAndDelay(`All branch retrieved`)
 
   if (branch.includes("types")) {
     await Git.switch("types")
-
     logAndDelay(`Branch switched to types`)
 
     await Git.pull({ force: true })
-
     logAndDelay(`Branch types pulled`)
 
   } else {
     await Git.switch("types", { orphan: true })
-
     logAndDelay(`Branch types created`)
   }
   logProcess(`Switched to types branch`)
 
-  await Bun.write("src/types/index.ts", types)
-
+  await Bun.write("src/index.ts", types)
   logAndDelay(`Types written to types branch`)
 
   await Bun.write(".gitignore", "*\n!src/*\n!.gitignore\n!package.json\n") // Neccessary to ignore all (*) since switching branch would also include other gitingore files that are generated. This would prevent files getting moved to the new branch
-  
   logAndDelay(`Gitignore written to types branch`)
-  
-  await Bun.$`bunx tsc src/types/index.ts -d --emitDeclarationOnly --skipLibCheck`
+
+  await Bun.$`bunx tsc src/index.ts -d --emitDeclarationOnly --skipLibCheck`
 
   await logAndDelay(`Types built with tsc`)
-
   logProcess(`Types updated and built with tsc`)
 
   if (!await Bun.file("package.json").exists()) {
@@ -63,15 +55,12 @@ try {
 
 
   await Git.add(".")
-
   logAndDelay(`Files added to staging`)
 
   await Git.commit(`Update types`)
-
   logAndDelay(`Files commited`)
 
   await Git.push("origin", "types", { setUpstream: true })
-
   logAndDelay(`Branch pushed to remote`)
 
   logProcess(`Branch pushed to remote`)
