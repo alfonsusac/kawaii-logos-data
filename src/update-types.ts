33a27899d237633bf54a445237c1f6fc1e995d1a
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-05 22:40:31 +0700

    write package.json

--- src/update-types.ts
+++ src/update-types.ts
@@ -12,24 +12,32 @@ try {
     await Git.pull({ force: true })
   } else {
     await Git.switch("types", { orphan: true })
+    await Bun.write(`package.json`, `{
+  "name": "types",
+  "version": "1.0.0",
+  "description": "Types for the kawaii-logos-data",
+  "main": "./src/index.ts",
+  "types": "./src/index.ts"
+  "author": "alfonsusac",
+  "license": "MIT"
+}`)
   }
   logProcess(`Switched to types branch`)
 
   await Bun.write("src/types/index.ts", types)
-  await Bun.write(".gitignore", "*\n!src\n") // Neccessary to ignore all since switching branch would also include other gitingore files that are generated. This would prevent files getting moved to the new branch
+  await Bun.write(".gitignore", "*\n!src\npackage.json\n") // Neccessary to ignore all (*) since switching branch would also include other gitingore files that are generated. This would prevent files getting moved to the new branch
   await Bun.$`bunx tsc src/types/index.ts -d --emitDeclarationOnly --skipLibCheck`
   await Git.add(".")
   await Git.commit(`Update types`)
   await Git.push("origin", "types", { setUpstream: true })
-  await Git.switch("main")
-  logProcess(`Switched back to main branch`)
 
 } catch (error) {
   logError(`Error occurred while updating data branch`)
   console.log(error)
 
 } finally {
-  Git.switch("main", {
+  await Git.switch("main", {
     force: true,
   })
+  logProcess(`Switched back to main branch`)
 }
