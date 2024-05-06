f9b2b357ea672b67367fae7109521029aaea8f85
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-06 21:54:53 +0700

    added imphen

---
+++ src/config/manual/urielchan.ts
@@ -0,0 +1,18 @@
+import { GitHub } from "../../url-github"
+import type { Entries } from "../../../types"
+
+export const urielchan: Entries[number] = {
+  handleName: "urielchan",
+  pfp: "https://avatars.githubusercontent.com/urielchan",
+  link: {
+    github: "urielchan",
+  },
+  repository: "https://github.com/Urielchan/logosVtuberVER",
+  images: [
+    {
+      title: "imphnen.png",
+      imgSrc: 'https://raw.githubusercontent.com/Urielchan/logosVtuberVER/main/imphnen%202.png',
+      source: 'https://github.com/Urielchan/logosVtuberVER/blob/main/imphnen%202.png',
+    },
+  ]
+}
