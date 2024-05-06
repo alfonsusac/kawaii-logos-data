74266bfa137e44ab4918d0e5e053eaa5fd7ea3d4
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-06 21:30:47 +0700

    added skill issue

---
+++ src/config/manual/alfonsusac.ts
@@ -0,0 +1,23 @@
+import { GitHub } from "../../url-github"
+import type { Entries } from "../../../types"
+
+export const disphing: Entries[number] = {
+  handleName: "alfonsusac",
+  pfp: "https://avatars.githubusercontent.com/alfonsusac",
+  link: {
+    github: "alfonsusac",
+    twitter: "alfonsusac",
+  },
+  repository: "https://github.com/alfonsusac/kawaii-logos-data/tree/main",
+  license: {
+    label: "CC BY-NC-SA 4.0",
+    href: "https://creativecommons.org/licenses/by-nc-sa/4.0/"
+  },
+  images: [
+    {
+      title: "Skill Issue",
+      imgSrc: GitHub.selfHostedStaticAssetUrl("skillissue.svg"),
+      source: 'https://github.com/alfonsusac/kawaii-logos-data/blob/main/assets/skillissue.svg',
+    },
+  ]
+}
