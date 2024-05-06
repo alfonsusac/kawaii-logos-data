c8fbfa0ecaeda67816c72577652bfd2919e9c2d5
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-06 21:36:41 +0700

    dshpng obejctfit

--- src/config/manual/disphing.ts
+++ src/config/manual/disphing.ts
@@ -13,26 +13,31 @@ export const disphing: Entries[number] = {
       title: "Skrillex",
       imgSrc: GitHub.selfHostedStaticAssetUrl("skrillexsticker.png"),
       source: "https://drive.google.com/file/d/1mumHSFG6k8O1uKG29rYc9s0Wm_jQDSpP/view?usp=sharing",
+      objectFit: "contain",
     },
     {
       title: "Porter Robinson",
       imgSrc: GitHub.selfHostedStaticAssetUrl("porterrobinsonsticker.png"),
       source: "https://drive.google.com/file/d/19bMQzV2ZseW-pyV8NSjDyitiuyeJNJ1G/view?usp=sharing",
+      objectFit: "contain",
     },
     {
       title: "FL Studio",
       imgSrc: GitHub.selfHostedStaticAssetUrl("flsticker.png"),
       source: "https://drive.google.com/file/d/1wMi3TOs-egixz6wuf4NNJpMfzArMcZZ2/view?usp=sharing",
+      objectFit: "contain",
     },
     {
       title: "Bitwig Studio",
       imgSrc: GitHub.selfHostedStaticAssetUrl("bitwigsticker.png"),
       source: "https://drive.google.com/file/d/1Dc5icFqOCOm_6qyZh1MxsFN4QtmI6RcH/view?usp=sharing",
+      objectFit: "contain",
     },
     {
       title: "Ableton Live",
       imgSrc: GitHub.selfHostedStaticAssetUrl("abletonsticker.png"),
       source: "https://drive.google.com/file/d/1HkvgRiaexWQ2TssE2ZSatUsAm5ZhGLTh/view?usp=sharing",
+      objectFit: "contain",
     },
     {
       title: "Logic Pro",
