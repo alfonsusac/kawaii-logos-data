687e29d4f07dc0503179c5cf50f196e1557346f7
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-06 21:25:11 +0700

    update dishping

--- src/config/manual/disphing.ts
+++ src/config/manual/disphing.ts
@@ -11,27 +11,27 @@ export const disphing: Entries[number] = {
   images: [
     {
       title: "Skrillex",
-      imgSrc: GitHub.selfHostedStaticAssetUrl("GLh5aGWakAAM3dk.jpeg"),
+      imgSrc: GitHub.selfHostedStaticAssetUrl("skrillexsticker.png"),
       source: "https://drive.google.com/file/d/1mumHSFG6k8O1uKG29rYc9s0Wm_jQDSpP/view?usp=sharing",
     },
     {
       title: "Porter Robinson",
-      imgSrc: GitHub.selfHostedStaticAssetUrl("GLh9h3rbQAA7wik.jpeg"),
+      imgSrc: GitHub.selfHostedStaticAssetUrl("porterrobinsonsticker.png"),
       source: "https://drive.google.com/file/d/19bMQzV2ZseW-pyV8NSjDyitiuyeJNJ1G/view?usp=sharing",
     },
     {
       title: "FL Studio",
-      imgSrc: GitHub.selfHostedStaticAssetUrl("GLhep0Za8AAyBff.jpeg"),
+      imgSrc: GitHub.selfHostedStaticAssetUrl("flsticker.png"),
       source: "https://drive.google.com/file/d/1wMi3TOs-egixz6wuf4NNJpMfzArMcZZ2/view?usp=sharing",
     },
     {
       title: "Bitwig Studio",
-      imgSrc: GitHub.selfHostedStaticAssetUrl("GLhevYka0AAmU7e.jpeg"),
+      imgSrc: GitHub.selfHostedStaticAssetUrl("bitwigsticker.png"),
       source: "https://drive.google.com/file/d/1Dc5icFqOCOm_6qyZh1MxsFN4QtmI6RcH/view?usp=sharing",
     },
     {
       title: "Ableton Live",
-      imgSrc: GitHub.selfHostedStaticAssetUrl("GLhesuxaYAAqS__.jpeg"),
+      imgSrc: GitHub.selfHostedStaticAssetUrl("abletonsticker.png"),
       source: "https://drive.google.com/file/d/1HkvgRiaexWQ2TssE2ZSatUsAm5ZhGLTh/view?usp=sharing",
     },
     {
