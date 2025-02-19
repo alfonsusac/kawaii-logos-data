ecd5c3a4e902648164a3aef1819e648e2b8b193a
Alfonsus Ardani <alfonsusac@gmail.com>
2025-02-19 19:39:53 +0700

    added arnav_kr entry

---
+++ src/config/manual/arnav.ts
@@ -0,0 +1,28 @@
+import type { Entries } from "../../../types"
+
+export const arnav: Entries[number] = {
+  handleName: "arnav-kr",
+  pfp: "https://avatars.githubusercontent.com/arnav-kr",
+  link: {
+    github: "arnav-kr",
+    twitter: "arnavkr_",
+  },
+  groups: [
+    {
+      name: "JSON Formatter",
+      files: [
+        {
+          title: "JF_VTuber_logo.png",
+          source: 'https://github.com/arnav-kr/json-formatter/blob/main/images/banners/JF_VTuber_logo.png',
+          imgSrc: 'https://raw.githubusercontent.com/arnav-kr/json-formatter/refs/heads/main/images/banners/JF_VTuber_logo.png',
+        },
+        {
+          title: "JF_VTuber_logo.svg",
+          source: 'https://github.com/arnav-kr/json-formatter/blob/main/images/banners/JF_VTuber_logo.svg',
+          imgSrc: 'https://raw.githubusercontent.com/arnav-kr/json-formatter/refs/heads/main/images/banners/JF_VTuber_logo.svg',
+        },
+
+      ]
+    }
+  ]
+}
