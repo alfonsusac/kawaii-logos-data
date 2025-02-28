f8b8f50325c46d06e108ce28a010c9d9389dc7da
Alfonsus Ardani <alfonsusac@gmail.com>
2025-02-28 10:16:46 +0700

    feat: added maxshawnwx

---
+++ src/config/manual/maxshawnwx.ts
@@ -0,0 +1,17 @@
+import type { Entries } from "../../../types"
+import { GitHub } from "../../lib/url-github"
+
+export const urielchan: Entries[number] = {
+  handleName: "maxshawnwx",
+  pfp: "https://avatars.githubusercontent.com/maxshawnwx",
+  link: {
+    github: "maxshawnwx",
+  },
+  images: [
+    {
+      title: "Skyviewer",
+      imgSrc: GitHub.selfHostedStaticAssetUrl("417108050-37bf72ec-404f-494c-862c-dae36d6bf022.png"),
+      source: "https://github.com/alfonsusac/kawaii-logos-data/issues/10#issuecomment-2686987751",
+    },
+  ]
+}
