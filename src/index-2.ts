0120456ddd334c16f155715b4b7272908a136cb5
Alfonsus Ardani <alfonsusac@gmail.com>
2026-04-21 22:43:28 +0700

    update script

--- src/index-2.ts
+++ src/index-2.ts
@@ -42,8 +42,8 @@ try {
   })
 
   const output = await prepareOutput(resolved)
-  await cleanAndSaveToDisk(output, "./dist")
-  await saveToDataBranch(output, "main-2", "main-2-data")
+  await cleanAndSaveToDisk(output, "./dist", true)
+  await saveToDataBranch(output, "main-2-data")
 
   log.success("Data processed successfully")
 } catch (error) {
@@ -95,11 +95,11 @@ type DataResponse = Awaited<ReturnType<typeof prepareOutput>>
 
 
 
-async function cleanAndSaveToDisk(data: DataResponse, folderpath: string) {
+async function cleanAndSaveToDisk(data: DataResponse, folderpath: string, clean: boolean) {
   const log = logger("saveToDisk")
   log.info(`Saving Data to ${ folderpath }`)
 
-  await rm(folderpath, { recursive: true, force: true })
+  clean && await rm(folderpath, { recursive: true, force: true })
   await Promise.all(Object.entries(data.folderStructure).map(([ path, content ]) => {
     return Bun.write(`${ folderpath }${ path }`, content)
   }))
@@ -107,56 +107,106 @@ async function cleanAndSaveToDisk(data: DataResponse, folderpath: string) {
 }
 
 
-async function saveToDataBranch(data: DataResponse, mainBranchName: string, dataBranchName: string) {
-  const log = logger("saveToDataBranch")
-  log.info(`Saving Data to branch ${ dataBranchName }`)
+async function saveToDataBranch(data: DataResponse, dataBranchName: string) {
+  await usingGitBranch(
+    dataBranchName,
+    async () => {
+      await cleanAndSaveToDisk(data, "./dist", true)
 
-  const branch = await Bun.$`git branch --show-current -a`.text()
+    }
+  )
+  // const log = logger("saveToDataBranch")
+  // log.info(`Saving Data to branch ${ dataBranchName }`)
+
+  // const branch = await Bun.$`git branch --show-current -a`.text()
+  // try {
+  //   if (branch.includes(dataBranchName)) {
+  //     log.verbose('git switch ${ dataBranchName }')
+  //     const gitswitch = await Bun.$`git switch ${ dataBranchName }`.nothrow()
+  //     if (gitswitch.stderr.toString().includes("Please commit your changes or stash them before you switch branches.")) {
+  //       log.error(`Local changes detected. Please commit or stash your changes before running the script.`)
+  //       return
+  //     }
+
+  //     log.verbose('git pull')
+  //     await Bun.$`git pull`
+  //   } else {
+  //     log.verbose('git switch --orphan ${ dataBranchName }')
+  //     const gitswitch = await Bun.$`git switch --orphan ${ dataBranchName }`.nothrow()
+
+  //     if (gitswitch.stderr.toString().includes("Please commit your changes or stash them before you switch branches.")) {
+  //       log.error(`Local changes detected. Please commit or stash your changes before running the script.`)
+  //       return
+  //     }
+  //   }
+  //   log.verbose(`Switched to branch ${ dataBranchName }`)
+  //   const currentBranch = await Bun.$`git branch --show-current`.text()
+  //   if (currentBranch.trim() !== dataBranchName) {
+  //     log.error(`Failed to switch to branch ${ dataBranchName }. Current branch is ${ currentBranch }`)
+  //     return
+  //   }
+
+  //   log.verbose(`Saving data to disk in branch ${ dataBranchName }`)
+  //   await cleanAndSaveToDisk(data, "./")
+
+  //   log.verbose('git add .')
+  //   await Bun.$`git add .`
+
+  //   log.verbose(`git commit -m "Update data ${ data.response.updatedAt }" -a`)
+  //   await Bun.$`git commit -m "Update data ${ data.response.updatedAt }" -a`
+
+  //   log.verbose(`git push -u origin ${ dataBranchName }`)
+  //   await Bun.$`git push -u origin ${ dataBranchName }`
+
+  //   log.verbose(`git switch ${ mainBranchName }`)
+  //   await Bun.$`git switch ${ mainBranchName }`
+
+  // } catch (error) {
+  //   log.error(`Error occurred while saving to data branch`, error)
+  //   log.verbose(`git switch ${ mainBranchName } --force`)
+  //   await Bun.$`git switch ${ mainBranchName } --force`
+  // }
+}
+
+
+
+async function usingGitBranch(
+  dataBranchName: string,
+  callback: () => Promise<void>
+) {
+  const log = logger("usingGitBranch")
+
+  const currentBranch = await Bun.$`git branch --show-current`.text()
+  const branches = (await Bun.$`git branch --format="%(refname:short)" -a`.text())
+    .split('\n')
+    .filter(branch => branch.trim() !== '')
+  const hasDataBranch = branches.includes(dataBranchName)
+  const hasUncommitedChanges = (await Bun.$`git status --porcelain`).text().trim() !== ''
+  if (hasUncommitedChanges) {
+    log.error(`Local changes detected. Please commit or stash your changes before running the script.`)
+    return
+  }
   try {
-    if (branch.includes(dataBranchName)) {
-      log.verbose('git switch ${ dataBranchName }')
-      const gitswitch = await Bun.$`git switch ${ dataBranchName }`.nothrow()
-      if (gitswitch.stderr.toString().includes("Please commit your changes or stash them before you switch branches.")) {
-        log.error(`Local changes detected. Please commit or stash your changes before running the script.`)
-        return
-      }
-
-      log.verbose('git pull')
+    if (hasDataBranch) {
+      log.verbose(`git switch ${ dataBranchName }`)
+      await Bun.$`git switch ${ dataBranchName }`
+      log.verbose(`git pull`)
       await Bun.$`git pull`
     } else {
-      log.verbose('git switch --orphan ${ dataBranchName }')
-      const gitswitch = await Bun.$`git switch --orphan ${ dataBranchName }`.nothrow()
-
-      if (gitswitch.stderr.toString().includes("Please commit your changes or stash them before you switch branches.")) {
-        log.error(`Local changes detected. Please commit or stash your changes before running the script.`)
-        return
-      }
+      log.verbose(`git switch --orphan ${ dataBranchName }`)
+      await Bun.$`git switch --orphan ${ dataBranchName }`
     }
-    log.verbose(`Switched to branch ${ dataBranchName }`)
-    const currentBranch = await Bun.$`git branch --show-current`.text()
-    if (currentBranch.trim() !== dataBranchName) {
-      log.error(`Failed to switch to branch ${ dataBranchName }. Current branch is ${ currentBranch }`)
-      return
-    }
-
-    log.verbose(`Saving data to disk in branch ${ dataBranchName }`)
-    await cleanAndSaveToDisk(data, "./")
-
-    log.verbose('git add .')
-    await Bun.$`git add .`
-
-    log.verbose(`git commit -m "Update data ${ data.response.updatedAt }" -a`)
-    await Bun.$`git commit -m "Update data ${ data.response.updatedAt }" -a`
-
-    log.verbose(`git push -u origin ${ dataBranchName }`)
-    await Bun.$`git push -u origin ${ dataBranchName }`
-
-    log.verbose(`git switch ${ mainBranchName }`)
-    await Bun.$`git switch ${ mainBranchName }`
-
   } catch (error) {
-    log.error(`Error occurred while saving to data branch`, error)
-    log.verbose(`git switch ${ mainBranchName } --force`)
-    await Bun.$`git switch ${ mainBranchName } --force`
+    log.error(`Error occurred while switching to data branch`, error)
+    log.verbose(`git switch ${ currentBranch } --force`)
+    await Bun.$`git switch ${ currentBranch } --force`
+    return
+  }
+  log.verbose(`Switched to branch ${ dataBranchName }`)
+  try {
+    await callback()
+  } finally {
+    log.verbose(`git switch ${ currentBranch } --force`)
+    await Bun.$`git switch ${ currentBranch } --force`
   }
 }
