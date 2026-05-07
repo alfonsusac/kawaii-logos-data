f44127fdc0b0fe4579ddaff82a142df190a8ecd0
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-07 20:22:40 +0700

    update funding

--- src/definitions/syke9p3.ts
+++ src/definitions/syke9p3.ts
@@ -1,6 +1,9 @@
 import type { AuthorDefinition } from "../resolve-author"
 
 export const syke9p3: AuthorDefinition = {
+  socials: {
+    site: "https://syke9p3.netlify.app",
+  },
   source: {
     from: "github",
     repo: "https://github.com/syke9p3/Syke-VTuber-Icons",
