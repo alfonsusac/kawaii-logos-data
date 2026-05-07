aabaddc97dc38eb62e52678614ffc45641c637d2
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-07 21:30:38 +0700

    rename fix

--- src/app.ts
+++ src/app.ts
@@ -32,9 +32,9 @@ import { revalidateMainWebsite } from "./revalidate"
 import type { KawaiiLogoData } from "./output"
 import { petricat } from "./definitions/petricat"
 import { ifalfahri } from "./definitions/ifalfahri"
-import { c29h25n3o5 } from "./definitions/_c29h25n3o5"
 import { anwar_achilles } from "./definitions/anwar-achilles"
 import { shiroiame_kusu } from "./definitions/shiroiame-kusu"
+import { c29h25n3o5 } from "./definitions/C29H25N3O5"
 
 
 runApp(async () => {
