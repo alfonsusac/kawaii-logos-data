5f04aa1ca43a270783df5f49275ec2714a989c9a
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-13 00:27:32 +0700

    add cnanoical domain

---
+++ src/domain.ts
@@ -0,0 +1,2 @@
+export const frontendDomain = "https://vtuberlogos.alfon.dev"
+// todo: change this to kawaiilogos.alfon.dev
--- src/revalidate.ts
+++ src/revalidate.ts
@@ -1,7 +1,8 @@
+import { frontendDomain } from "./domain"
 import { isInGitHubAction, revalidateToken } from "./env"
 
 export async function revalidateMainWebsite() {
   if (isInGitHubAction && revalidateToken) {
-    fetch(`https://vtuberlogos.alfon.dev/revalidate?key=${ revalidateToken }`).then((res) => res.json())
+    fetch(`${ frontendDomain }/revalidate?key=${ revalidateToken }`).then((res) => res.json())
   }
 }
