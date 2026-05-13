211b00e569e5ab7187e2a6f7dfcfe8c4fffa84f1
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-13 14:31:59 +0700

    update revalidate urls

--- src/domain.ts
+++ src/domain.ts
@@ -1,3 +1,4 @@
 // export const frontendDomain = "https://vtuberlogos.alfon.dev"
 export const frontendDomain = "https://kawaiilogos.alfon.dev"
+export const frontendPreviewDomain = "https://kawaiilogos-preview-dev.alfon.dev"
 // todo: change this to kawaiilogos.alfon.dev
--- src/revalidate.ts
+++ src/revalidate.ts
@@ -1,9 +1,20 @@
-import { frontendDomain } from "./domain"
+import { frontendDomain, frontendPreviewDomain } from "./domain"
 import { isInGitHubAction, revalidateToken } from "./env"
 
 export async function revalidateMainWebsite() {
   if (isInGitHubAction && revalidateToken) {
     fetch(`${ frontendDomain }/revalidate`, {
+      body: JSON.stringify({
+        token: revalidateToken,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    }).then((res) => res.json()).catch((err) => {
+      console.error("Failed to revalidate main website:", err)
+    })
+    fetch(`${ frontendPreviewDomain }/revalidate`, {
       body: JSON.stringify({
         token: revalidateToken,
       }),
