c2b8ef368465c60a9e7676310c6458e84800765f
Alfonsus Ardani <alfonsusac@gmail.com>
2026-08-14 22:04:15 +0700

    added kane's

---
+++ src/content/kane50613.ts
@@ -0,0 +1,22 @@
+import type { AuthorDefinition } from "../resolve-author"
+
+export const kane50613: AuthorDefinition = {
+  socials: {
+    x: "https://x.com/kanewang_",
+    github: "https://github.com/kane50613/",
+    site: "https://kane.tw",
+  },
+  entries: {
+    'takumi': {
+      label: "Takumi",
+      images: [
+        { src: "https://github.com/kane50613/takumi/blob/master/assets/images/sticker.svg" },
+      ],
+      createdAt: "Jul 19, 2026",
+      references: [
+        "official:https://takumi.kane.tw/",
+        "https://x.com/kanewang_/status/2078838378301403254",
+      ]
+    }
+  }
+}
