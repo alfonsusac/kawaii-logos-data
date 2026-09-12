80a58e9c4247a59614ffeb353cc10c77aa414eb2
Aureal <nonnewport@proton.me>
2026-09-12 02:01:17 -0400

    Add new author definition for NPSummers

---
+++ src/content/npsummers.ts
@@ -0,0 +1,76 @@
+import type { AuthorDefinition } from "../resolve-author"
+
+export const aureal: AuthorDefinition = {
+  socials: {
+    x: "https://x.com/nonnewport",
+    github: "https://github.com/NPSummers",
+    site: "https://aureal.dev",
+  },
+  entries: {
+    'aurealdev': {
+      label: "aureal.dev",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/aureal.png?raw=true" },
+      ],
+      createdAt: "Sept 12, 2026",
+      references: [
+        "https://aureal.dev",
+      ],
+      license: "All Rights Reserved",
+    }
+  },
+  {
+    'mxrzaxyz': {
+      label: "mxrza.xyz",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/mizi.png?raw=true" },
+      ],
+      createdAt: "Sept 12, 2026",
+      references: [
+        "https://mxrza.xyz/",
+        "https://en.wikipedia.org/wiki/Serial_Experiments_Lain",
+      ],
+      license: "All Rights Reserved",
+    }
+  },
+  {
+    'anthonnyfyi': {
+      label: "anthonny.fyi",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Anthonny.png?raw=true" },
+      ],
+      createdAt: "Sept 12, 2026",
+      references: [
+        "https://anthonny.fyi/",
+      ],
+      license: "All Rights Reserved",
+    }
+  },
+  {
+    'lanyard': {
+      label: "lanyard.rest",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/lanyard.png?raw=true" },
+      ],
+      createdAt: "Sept 12, 2026",
+      references: [
+        "https://lanyard.rest/",
+        "https://lanyard.cafe/",
+      ],
+      license: "MIT",
+    }
+  },
+  {
+    'abuseipdb': {
+      label: "abuseipdb.com",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/AbuseIPDB.png?raw=true" },
+      ],
+      createdAt: "Sept 12, 2026",
+      references: [
+        "https://www.abuseipdb.com/",
+      ],
+      license: "MIT",
+    }
+  }  
+}
