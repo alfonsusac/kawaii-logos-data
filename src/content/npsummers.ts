7051a8a669716da30bc06ed2e856ee27c10c7c1a
Aureal <nonnewport@proton.me>
2026-09-12 16:19:10 -0400

    Add funding information and new entries

--- src/content/npsummers.ts
+++ src/content/npsummers.ts
@@ -6,6 +6,9 @@ export const aureal: AuthorDefinition = {
     github: "https://github.com/NPSummers",
     site: "https://aureal.dev",
   },
+  fundings: [
+    { type: "github", url: "https://github.com/sponsors/NPSummers" }
+  ],
   entries: {
     'aurealdev': {
       label: "aureal.dev",
@@ -65,6 +68,97 @@ export const aureal: AuthorDefinition = {
       createdAt: "Sep 12, 2026",
       references: [
         "official:https://www.abuseipdb.com/",
+      ],
+      license: {
+        type: "MIT"
+      },
+    },
+    'akrystmoe': {
+      label: "akryst.moe",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Akryst.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://akryst.moe/",
+      ],
+      license: {
+        type: "All Rights Reserved"
+      },
+    },
+    'alfon': {
+      label: "Alfon.dev",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Alfon.dev.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://alfon.dev/",
+      ],
+      license: {
+        type: "All Rights Reserved"
+      },
+    },
+    'Anthropic': {
+      label: "Anthropic",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Claude.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://www.anthropic.com/",
+      ],
+      license: {
+        type: "MIT"
+      },
+    },
+    'KawaiiServiceLogos': {
+      label: "Kawaii Service Logos",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Kawaii.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://kawaiilogos.alfon.dev/",
+      ],
+      license: {
+        type: "MIT"
+      },
+    },
+    'Signal': {
+      label: "Signal",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Signal.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://signal.org/",
+      ],
+      license: {
+        type: "MIT"
+      },
+    },
+    'EnkaNetwork': {
+      label: "Enka.Network",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/enka.network.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://enka.network/",
+      ],
+      license: {
+        type: "MIT"
+      },
+    },
+    'PostgreSQL': {
+      label: "PostgreSQL",
+      images: [
+        { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/postgresql.png" },
+      ],
+      createdAt: "Sep 12, 2026",
+      references: [
+        "official:https://www.postgresql.org/",
       ],
       license: {
         type: "MIT"
