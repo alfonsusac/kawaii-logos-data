4fd3a0deead80d379fb58e0e74f9b444e7ce5fb9
Alfonsus Ardani <alfonsusac@gmail.com>
2026-09-13 11:51:59 +0700

    update references

--- src/content/npsummers.ts
+++ src/content/npsummers.ts
@@ -16,9 +16,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/aureal.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://aureal.dev",
-      ],
+      // references: [
+      //   "official:https://aureal.dev",
+      // ],
     },
     'mxrzaxyz': {
       label: "mxrza.xyz",
@@ -26,9 +26,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/mizi.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://mxrza.xyz/",
-      ],
+      // references: [
+      //   "official:https://mxrza.xyz/",
+      // ],
       license: {
         type: "All Rights Reserved"
       },
@@ -39,9 +39,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Anthonny.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://anthonny.fyi/",
-      ],
+      // references: [
+      //   "official:https://anthonny.fyi/",
+      // ],
       license: {
         type: "All Rights Reserved"
       },
@@ -52,10 +52,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/lanyard.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://lanyard.rest/",
-        "official:https://lanyard.cafe/",
-      ],
+      // references: [
+      //   "official:https://lanyard.cafe/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -66,9 +65,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/AbuseIPDB.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://www.abuseipdb.com/",
-      ],
+      // references: [
+      //   "official:https://www.abuseipdb.com/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -79,9 +78,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Akryst.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://akryst.moe/",
-      ],
+      // references: [
+      //   "official:https://akryst.moe/",
+      // ],
       license: {
         type: "All Rights Reserved"
       },
@@ -92,9 +91,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Alfon.dev.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://alfon.dev/",
-      ],
+      // references: [
+      //   "official:https://alfon.dev/",
+      // ],
       license: {
         type: "All Rights Reserved"
       },
@@ -105,9 +104,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Claude.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://www.anthropic.com/",
-      ],
+      // references: [
+      //   "official:https://www.anthropic.com/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -118,9 +117,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Kawaii.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://kawaiilogos.alfon.dev/",
-      ],
+      // references: [
+      //   "official:https://kawaiilogos.alfon.dev/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -131,9 +130,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/Signal.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://signal.org/",
-      ],
+      // references: [
+      //   "official:https://signal.org/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -144,9 +143,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/enka.network.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://enka.network/",
-      ],
+      // references: [
+      //   "official:https://enka.network/",
+      // ],
       license: {
         type: "MIT"
       },
@@ -157,9 +156,9 @@ export const aureal: AuthorDefinition = {
         { src: "https://github.com/NPSummers/NPSummers/blob/main/logos/postgresql.png" },
       ],
       createdAt: "Sep 12, 2026",
-      references: [
-        "official:https://www.postgresql.org/",
-      ],
+      // references: [
+      //   "official:https://www.postgresql.org/",
+      // ],
       license: {
         type: "MIT"
       },
